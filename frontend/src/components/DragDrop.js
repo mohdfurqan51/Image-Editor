a294675550a6eef38bf3d3ab7f2250b0716e018a
mohdfurqan51 <mohdfurqan23789@gmail.com>
2022-07-28 18:18:54 +0530

    in

--- frontend/src/components/DragDrop.js
+++ frontend/src/components/DragDrop.js
@@ -1,36 +1,36 @@
-import React from 'react'
-import "./css/dragdrop.css"
+// import React from 'react'
+// import "./css/dragdrop.css"
 
-const DragDrop = () => {
-  return (
+// const DragDrop = () => {
+//   return (
    
-    <div className="upload">
- <div className="upload-files">
-  <header>
-   <p>
-    <i className="fa fa-cloud-upload" aria-hidden="true"></i>
-    <span className="up">up</span>
-    <span className="load">load</span>
-   </p>
-  </header>
-  <div className="body" id="drop">
-   <i className="fa fa-file-text-o pointer-none" aria-hidden="true"></i>
-   <p className="pointer-none"><b>Drag and drop</b> files here <br /> or <a href="" id="triggerFile">browse</a> to begin the upload</p>
-			<input type="file" multiple="multiple" />
-  </div>
-  <footer>
-   <div className="divider">
-    <span>FILES</span>
-   </div>
-   <div className="list-files">
+//     <div className="upload">
+//  <div className="upload-files">
+//   <header>
+//    <p>
+//     <i className="fa fa-cloud-upload" aria-hidden="true"></i>
+//     <span className="up">up</span>
+//     <span className="load">load</span>
+//    </p>
+//   </header>
+//   <div className="body" id="drop">
+//    <i className="fa fa-file-text-o pointer-none" aria-hidden="true"></i>
+//    <p className="pointer-none"><b>Drag and drop</b> files here <br /> or <a href="" id="triggerFile">browse</a> to begin the upload</p>
+// 			<input type="file" multiple="multiple" />
+//   </div>
+//   <footer>
+//    <div className="divider">
+//     <span>FILES</span>
+//    </div>
+//    <div className="list-files">
     
-   </div>
-		<button className="importar">UPDATE FILES</button>
-  </footer>
- </div>
-</div>
+//    </div>
+// 		<button className="importar">UPDATE FILES</button>
+//   </footer>
+//  </div>
+// </div>
 
-  )
-}
+//   )
+// }
 
-export default DragDrop
+// export default DragDrop
